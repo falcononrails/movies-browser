7966c585d94490b83ff0b98852093f186007f2b2
Anas Limouri <tsudete@gmail.com>
2021-12-02 13:37:05 +0100

    feat: Added title tag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Head from "next/head";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>{"Movies Browser"}</title>
+      </Head>
       <div className={"bg-gray-900 w-full flex flex-col"}>
         <Navbar />
-        <div className="bg-gray-900 text-gray-100">
-          {children}
-        </div>
+        <div className="bg-gray-900 text-gray-100">{children}</div>
         <Footer />
       </div>
     </>
