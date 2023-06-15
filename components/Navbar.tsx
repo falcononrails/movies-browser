57c45f6691ccbb95c83a4c109285708ee240d3fc
Anas LIMOURI <tsudete@gmail.com>
2023-06-16 00:27:47 +0200

    Change logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,16 +3,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
-
   return (
     <nav className="sticky-nav flex justify-between p-4">
       <Link href="/">
         <a className="flex justify-center items-center">
           <Image
-            src="/logos/canalplus.png"
-            alt="CanalPlus"
-            width={175}
-            height={45  }
+            src="/logos/streamberry_logo.png"
+            alt="Streamberry"
+            width={399}
+            height={86}
           />
         </a>
       </Link>
